5f452474e52069c6b3444fbc293f6a169fb0bd6f
Rajendra <rajendraku6711@gmail.com>
2024-07-28 17:22:35 +0530

    Splash Screen

--- news_app/lib/main.dart
+++ news_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:news_app/providers/news_provider.dart';
-import 'package:news_app/screens/home_screen.dart';
+import 'package:news_app/screens/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: HomeScreen(),
+        home: SplashScreen(),
       ),
     );
   }
